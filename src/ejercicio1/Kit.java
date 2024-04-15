74f1fa3e3bbd09958507ea599baecdb6fa75b376
Sira <sgonzmad@myuax.com>
2024-04-15 14:46:01 +0200

    terminar clase compuesta que representa un kit

--- src/ejercicio1/Kit.java
+++ src/ejercicio1/Kit.java
@@ -6,12 +6,25 @@ import java.util.List;
 //Clase compuesta que representa un kit
 public class Kit extends Elemento {
     private List<Elemento> elementos = new ArrayList<>();
-    public Kit(double precio, int codigo) {
-        super(precio, codigo);
+    public Kit(int codigo) {
+        super(0, codigo); //El precio se calculará dinámicamente
+    }
+
+    public void agregarElemento(Elemento elemento) {
+        elementos.add(elemento);
+    }
+
+    public void eliminarElemento(Elemento elemento) {
+        elementos.remove(elemento);
     }
 
     @Override
     public double calcularPrecio() {
-        return getPrecio() * 2;
+        double precioTotal = 0;
+        for (Elemento elemento : elementos) {
+            precioTotal += elemento.calcularPrecio();
+        }
+        //Aplicar el descuento del 10% para los kits
+        return precioTotal * 0.9;
     }
 }

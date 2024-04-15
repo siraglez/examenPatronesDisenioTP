d84c016437005913700c3908d625b2f3821c0d17
Sira <sgonzmad@myuax.com>
2024-04-15 14:50:48 +0200

    crear elementos y kit, agregar elementos a kit

--- src/ejercicio1/Main.java
+++ src/ejercicio1/Main.java
@@ -1,4 +1,15 @@
 package ejercicio1;
 
 public class Main {
+    public static void main(String[] args) {
+        //Crear elementos simples
+        Elemento elemento1 = new ElementoSimple(100, 1);
+        Elemento elemento2 = new ElementoSimple(200, 2);
+
+        //Crear un kit y agregar elementos
+        Kit kit = new Kit(3);
+        kit.agregarElemento(elemento1);
+        kit.agregarElemento(elemento2);
+
+    }
 }
